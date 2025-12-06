7da3bdf15bb1c485ee759c77c09d1c5947d2076a
mike <michamytych@gmail.com>
2025-12-06 23:36:26 +0100

    Add Untitled UI & TypeScript support

---
+++ src/main.tsx
@@ -0,0 +1,24 @@
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { RouteProvider } from '@/providers/route-provider';
+import { ThemeProvider } from '@/providers/theme-provider';
+import App from '@/App';
+import '@/styles/globals.css';
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <RouteProvider>
+          <ThemeProvider>
+            <App />
+          </ThemeProvider>
+        </RouteProvider>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
